2763c110142a3d5843a4efb88ee7f5a559e26540
jlmorano <janelle.morano@cornell.edu>
2021-08-26 16:52:26 -0400

    added summaries

--- basicMenhaden with covariates.R
+++ basicMenhaden with covariates.R
@@ -12,6 +12,23 @@ head(data)
 # I am missing Area swept in these data, so I'm going to fudge it and add 0.01 to every row for now.
 data$AreaSwept_km2 <- 0.01
 
+
+# Make covariate data
+# It appears that habitat covariates must be in a separate df and must include columns `Lat`, `Lon`, and `Year`
+habitat_cov = data.frame(
+  "Lat" = data$LAT,
+  "Lon" = data$LON,
+  "Year" = data$YEAR,
+  "DEPTH" = data$DEPTH,
+  "BOTTEMP" = data$BOTTEMP
+)
+
+#Try setting year = NA
+habitat_cov$Year = NA
+
+# User Defined extrapolation grid
+user_region <- readRDS('user_region.rds')
+
 # Make settings (turning off bias.correct to save time for example)
 settings = make_settings( n_x = 250,
                           Region = 'User',
@@ -26,6 +43,7 @@ settings = make_settings( n_x = 250,
 # three degrees of freedom to model a nonlinear effect of log-transformed bottom depth,
 # based on example developed by Nicholas Ducharme-Barth.
 X1_formula = ~ bs( log(DEPTH), degree=2, intercept=FALSE)
+# X1_formula = ~ 0
 
 # Interaction
 #X2_formula = ~ poly(log(BOT_DEPTH), degree=2) + poly( BOT_TEMP, degree=2 )
@@ -34,33 +52,24 @@ X1_formula = ~ bs( log(DEPTH), degree=2, intercept=FALSE)
 #  then set Year = NA to cause values to be duplicated internally for all values of Year
 # If using a mix of static and dynamic covariates,
 #  then duplicate rows for static covariates for every value of Year
-# Here, all covariates are static, so I'm using the first approach.
-# Set Year to NA because covariate depth is static
-data$YEAR = NA
+# Here, depth covariates are static, so I'm using the first approach.
+habitat_cov$Year = NA
 
 # Rescale covariates being used to have an SD >0.1 and <10 (for numerical stability)
-data$DEPTH = data$DEPTH / 100
+habitat_cov$DEPTHscale = habitat_cov$DEPTH / 100
 
+# Model formula
+# as a cheat, for simplicity right here, each intercept parameter (ie Beta, Epsilon) is written as "s" sort of in the style of a GAM smoother
+# P(i) = s(c_i, t_i) + s(Lat_i, Lon_i
 
 # Run model
 fit = fit_model( "settings" = settings,
-                 Lat_i = example$sampling_data[,'Lat'],
-                 Lon_i = example$sampling_data[,'Lon'],
-                 t_i = example$sampling_data[,'Year'],
-                 b_i = example$sampling_data[,'Catch_KG'],
-                 a_i = example$sampling_data[,'AreaSwept_km2'],
+                 Lat_i = data$LAT,
+                 Lon_i = data$LON,
+                 t_i = data$YEAR, #time
+                 b_i = data$ABUNDANCE, #catch
+                 a_i = data$AreaSwept_km2, #area swept, This is FAKED and NOT REAL
                  X1_formula = X1_formula,
-                 X2_formula = X2_formula,
-                 covariate_data = example$covariate_data )
-
-fit = fit_model( "settings" = settings,
-                 "Lat_i" = data$LAT,
-                 "Lon_i" = data$LON,
-                 "t_i" = data$YEAR, #time
-                 "b_i" = data$ABUNDANCE, #catch
-                 "a_i" = data$AreaSwept_km2, #area swept, This is FAKED and NOT REAL
-                 "v_i"= data$SVVESSEL,
-                 X1_formula = X1_formula,
-                 covariate_data = data,
-                 "input_grid" = user_region)
+                 covariate_data = habitat_cov,
+                 input_grid = user_region)
 
--- basicmenhaden model in VAST.R
+++ basicmenhaden model in VAST.R
@@ -1,6 +1,6 @@
 ## Simple Menhaden model
 # Detailed context in in the "basic mehaden model in VAST.rmd"
-# This is the simplified code without much comment for error testing.
+# This is the simplified code that represents the latest (hopefully, working) version.
 
 ### Load packages
 library(VAST)
@@ -18,19 +18,21 @@ data <- read.csv("/Users/janellemorano/Git/Reference-R-scripts/VAST_exploration/
 data$AreaSwept_km2 <- 0.01
 
 ##-- Model Settings
-settings = make_settings( n_x = 100, #number of knots is arbitrary right now, keeping low to start
-                         Region = 'User', 
-                         purpose = "index2", 
-                         knot_method = 'grid',
-                         ObsModel= c(1,0),#, 1st value encounter probabilities = Poisson (7) lognormal (1), 2nd value catch rate= 0 default (the order is incorrect on the VAST input Google doc),
-                         bias.correct = FALSE )
-# Turn off temporal components
-#settings$FieldConfig[2,] <- 0
+settings = make_settings( n_x = 250, 
+                          Region = 'User', 
+                          purpose = "index2", 
+                          #strata.limits = example$strata.limits, #this was in the simple model example but not sure how it would interface here with a user-defined grid
+                          knot_method = 'grid',
+                          ObsModel= c(2,0),#, 1st value encounter probabilities = Poisson (7) lognormal (1), 2nd value catch rate= 0 default (the order is incorrect on the VAST input Google doc)),
+                          bias.correct = FALSE )
+#need to look more into next step
+settings$FieldConfig[2,] <- 0 ## turn off temporal components with 0
 
 ##-- User Defined extrapolation grid
 user_region <- readRDS('user_region.rds')
 
-##-- Fit the model
+# Fit the model
+# The model is predicting the encounter probability in the first step and the positive catch rate at each location in the second step where each are a function of the Year, Lat/Lon location, abundance, area swept, and vessel effects. This doesn't have any covariates (depth or bottom temp) yet.
 fit = fit_model( "settings" = settings,
                  "Lat_i" = data$LAT,
                  "Lon_i" = data$LON,
@@ -40,19 +42,8 @@ fit = fit_model( "settings" = settings,
                  "v_i"= data$SVVESSEL,
                  "input_grid" = user_region)
 
-#-- Errors in fitting
-# Getting the following errors:
-
-# NA/NaN function evaluationNA/NaN function evaluationNA/NaN function evaluationNA/NaN function evaluationNA/NaN function evaluationNA/NaN function evaluation 
-# Error in optimHess(parameter_estimates$par, fn = fn, gr = gr) : gradient in optim evaluated to length 1 not 110
-
-## after turning off the temporal components
-# Error in optimHess(parameter_estimates$par, fn = fn, gr = gr) : 
-#   gradient in optim evaluated to length 1 not 110
-# In addition: There were 19 warnings (use warnings() to see them)
-
-## switched from Poisson to lognormal distribution and it worked
-
-
-##-- Plot the results
+# Plot the results
+# First, set the working directory to save the plots to a spot where I won't GIT the output because of size limitations.
+setwd("/Users/janellemorano/Git/VAST output_DONOTGIT/_currentrun")
 plot( fit )
+
